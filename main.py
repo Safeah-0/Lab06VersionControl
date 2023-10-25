54a6b9b4c888ee3bae75f65bf39aba6c3bce037b
arjunmalkani <arjunmalkani@ufl.edu>
2023-10-25 14:56:48 -0400

    added decode function

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def encode_password(password):
     return encoded_password
 
 
+def decode_password(password):
+    decoded_password = ""
+    for digit in password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+
 
 # Main function
 def main():
@@ -17,20 +24,21 @@ def main():
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
+        print()  # whitespace
         choice = input("Please enter an option: ")
 
         if choice == "1":
             password = input("Please enter your password to encode: ")
             encoded_password = encode_password(password)
-            print(f"Your password has been encoded and stored!")
-        elif choice == "2":
-            encoded_password = input("Please enter the encoded password: ")
+            print(f"Your password has been encoded and stored!\n")
+        elif choice == "2":  # removed encoded_password variable assignment as no input is taken when choice 2 | ArjunM
             original_password = decode_password(encoded_password)
-            print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+            print(f"The encoded password is {encoded_password}, and the original password is {original_password}.\n")
         elif choice == "3":
             break
         else:
             print("Invalid option. Please select a valid option.")
+
 
 if __name__ == "__main__":
     main()
